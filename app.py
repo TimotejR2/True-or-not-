3f5801403a87357afa2ecbeff5b0c75e9be98c58
TimotejR <timotej.ruzicka@gmail.com>
2024-09-27 18:01:59 +0200

    add qr page that loads data from pages.json

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import time
 import datetime
 import datetime
+import json
 
 # Read the date string from the file
 with open('static/data/start_date.txt', 'r') as file:
@@ -15,3 +16,17 @@ def index():
         return render_template('countdown.html')
         
     return render_template('homepage.html')
+
+@app.route('/qr', methods=['GET'])
+def qr_scaned():
+    # return template data based on pages.json
+    with open('config/pages.json', 'r') as file:
+        code = request.args.get('code')
+        data = file.read()
+        pages = json.loads(data)['pages']
+        for page in pages:
+            if page['code'] == code:
+                return render_template('qr_scaned.html', **page)
+        
+    
+    return render_template('qr.html')
